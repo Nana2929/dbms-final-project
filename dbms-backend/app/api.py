725eb1e86d76792be487d8c4084589154cc43313
Nana2929 <b06102020@ntu.edu.tw>
2022-12-09 21:24:30 +0800

    add: router

--- dbms-backend/app/api.py
+++ dbms-backend/app/api.py
@@ -4,9 +4,25 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+datarows = [
+    {
+        "id": "1",
+        "item": "Read a book.",
+        "ssn":"P76114511",
+        "CourseTaking Count": 2
+    },
+    {
+        "id": "2",
+        "item": "Cycle around town.",
+        "ssn":"P76114511",
+        "CourseTaking Count": 4
+    }
+]
+
+
 origins = [
-    "http://localhost:3010",
-    "localhost:3010"
+    "http://localhost:3000",
+    "localhost:3000"
 ]
 app.add_middleware(
     CORSMiddleware,
@@ -16,9 +32,14 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
+@app.get("/todo", tags=["todos"])
+async def get_todos() -> dict:
+    return { "data": datarows }
 
-
-
-@app.get("/", tags=["root"])
-async def read_root() -> dict:
-    return {"message": "Welcome to IIU Student Management System."}
+@app.post("/todo", tags=["todos"])
+async def recv_command(command: dict) -> dict:
+    cmd = command["command"]
+    print(f'{cmd} button is pressed.' )
+    return {
+        "data": { f"Command {cmd} Received" }
+    }
